b40398956de278eb72c202417e750d475d011d52
Ziming Qu <>
2021-05-21 13:00:51 +0000

    comments

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -7,9 +7,13 @@ public class Recursion{
   public static int countHi(String str){
     if(str.length() < 2) {
       return 0;
+      //there is no way a hi in an array length < 2, base case
     }else if(str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+      //check if this part of string includes hi
 		  return 1 + countHi(str.substring(2)); 
+      //if yes, return 1 and sub the string from index 2, recursion case here
     } 
     return countHi(str.substring(1));
+    //if the string length >2 but there is no hi in first two index, move forward by one to check. so ther is no return values.
   }
 }
