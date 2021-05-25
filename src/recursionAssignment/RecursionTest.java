533b42e9d9156a4f190fc6cd8e379ac8f840c105
Ziming Qu <>
2021-05-25 03:44:28 +0000

    questuon 1 test finish, I forgot to write the test first. Sorry~

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,8 +1,8 @@
-            package recursionAssignment;
+package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
+import org.junit.Before;
+import org.junit.After;
 
 import recursionAssignment.Recursion;
 
@@ -21,6 +21,38 @@ public class RecursionTest{
         // make assertion statement(s)
         // assertEquals(EXPECTEDVALUE, TESTVALUE);
         // assertTrue(TESTBOOLEAN);
+        // first test before I write coode
+        assertEquals(1, Recursion.countHi("xxhixx"));
+    }
+
+    @Test
+    public void Test2(){
+        assertEquals(0, Recursion.countHi("xxHixx"));
+    }
+
+    @Test
+    public void Test3(){
+        assertEquals(2, Recursion.countHi("xxhihixx"));
+    }
+
+    @Test
+    public void Test4(){
+        assertEquals(2, Recursion.countHi("xxhixxhi"));
+    }
+
+    @Test
+    public void Test5(){
+        assertEquals(1, Recursion.countHi("xxhixxHi"));
+    }
+
+    @Test
+    public void Test6(){
+        assertEquals(0, Recursion.countHi("xxxx"));
+    }
+
+    @Test
+    public void Test7(){
+        assertEquals(1, Recursion.countHi("hi"));
     }
     
     // add more tests
